49bd8e2cfaeee42c24c44caa62bbcf7654a7e701
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-20 13:07:47 +0300

    1

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
+
+
+// 414aafe9b2b64898bc616f3296dc2df9
+
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTRhYWZlOWIyYjY0ODk4YmM2MTZmMzI5NmRjMmRmOSIsInN1YiI6IjYyNTU3MDljMWIxZjNjMGYzZDQ2NDgwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RD-NH0DaHCf5AYXhqonE8tRgwBU0QYupmcdlyF1CfHM
