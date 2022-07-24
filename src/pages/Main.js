d3c412d64c84cc39f336a2be8ddf47111a01077a
Muhammed Eroğlu <llofmejb@gmail.com>
2022-07-24 14:20:33 +0300

    3

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => {
+        setMovies(res.data.results);
+        console.log(movies);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div>Main</div>
-  )
-}
+    <>
+      <div className="d-flex justify-content-center flex-wrap">
+        {movies.map((movie) =>
+          // {...movie} şeklinde verdiğimizde içinden her şeyi alabiliriz. movie.id gibi uüraşmaya gerek yok
+          <MovieCard key={movie.id} {...movie} />
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Main
-
-
-  // const FEATURED_API =
-  //   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-  // // const IMG_API = "https://image.tmdb.org/t/p/w1280";
-  // const SEARCH_API =
-  //   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+export default Main;
